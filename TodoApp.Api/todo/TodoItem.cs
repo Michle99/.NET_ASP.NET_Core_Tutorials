4c4d0bb1643e2722885344773b1e6bd1bcef11b9
Michle99 <sammiyonyx@gmail.com>
2024-01-31 02:46:09 -0500

    added `Todo` namespace

--- TodoApp.Api/todo/TodoItem.cs
+++ TodoApp.Api/todo/TodoItem.cs
@@ -1,7 +1,11 @@
 // TodoItem.cs
+
+namespace Todo;
+
 public class TodoItem
 {
-    public int Id { get; set; }
-    public required string Title { get; set; }
-    public bool IsCompleted { get; set; }
+  public int Id { get; set; }
+  public required string Title { get; set; }
+  public bool IsCompleted { get; set; }
 }
+
