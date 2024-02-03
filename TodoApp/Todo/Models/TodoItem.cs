edcb6da0bad4b2baa5e3ef9faf738a4a6e321f74
Michle99 <sammiyonyx@gmail.com>
2024-02-03 04:42:51 -0500

    updated `TodoList` functions

--- TodoApp/Todo/Models/TodoItem.cs
+++ TodoApp/Todo/Models/TodoItem.cs
@@ -4,6 +4,6 @@ namespace TodoApp.Models;
 public class TodoItem
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+    public string Title { get; set; } = string.Empty;
     public bool IsCompleted { get; set; }
 }
